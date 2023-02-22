c810393a3bad1a845dc348fa0d8fc0983c8558f2
Ernesto Franklin <ing.efranklin7@gmail.com>
2023-02-22 13:20:36 -0800

    first commit

---
+++ Backend/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> users { get; set; }  
+    }
+}
---
+++ Backend/Model/User.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace Backend.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
